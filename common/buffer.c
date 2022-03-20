36548e6cc8efd1ac88f359010748ce334c1827c2
llwyd <tom@llwyd.io>
2022-03-20 23:22:07 +0000

    fixed bug in buffer code which prevents interrupt based tasks working and used task queue

--- common/buffer.c
+++ common/buffer.c
@@ -1,6 +1,6 @@
 #include "buffer.h" 
 
-#define BUFFER_SIZE ( 8U )
+#define BUFFER_SIZE ( 32U )
 
 typedef struct
 {
@@ -21,33 +21,39 @@ void Task_Init( void )
 
 void Task_Add( void ( *fn)( void ) )
 { 
-    /* Enter critical Section */
-    asm("CPSID IF");
    
     if( task.fill < BUFFER_SIZE )
     {
-        task.fn[task.write_index++] = fn;
+        task.fn[task.write_index] = fn;
+        
+        /* Enter critical Section */
+        asm("CPSID IF");
+        task.write_index++;
         task.fill++;
         task.write_index %= BUFFER_SIZE;
+    
+        /* Exit critical Section */
+        asm("CPSIE IF");
     } 
  
-    /* Exit critical Section */
-    asm("CPSIE IF");
 }
 
 void Task_Get( void )
 { 
-    /* Enter critical Section */
-    asm("CPSID IF");
    
     if( task.fill > 0 )
     {
-        task.fn[task.read_index++]();
+        task.fn[task.read_index]();
+
+        /* Enter critical Section */
+        asm("CPSID IF");
+        task.read_index++;
         task.fill--;
         task.read_index %= BUFFER_SIZE;
+    
+        /* Exit critical Section */
+        asm("CPSIE IF");
     } 
  
-    /* Exit critical Section */
-    asm("CPSIE IF");
 }
 
