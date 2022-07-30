b484e4c5edfe5f9a8b1637dbf3f7777eb231f228
llwyd <tom@llwyd.io>
2022-07-30 21:38:12 +0100

    using TCC0 as timer for better configurability

---
+++ common/timer.c
@@ -0,0 +1,42 @@
+#include "timer.h"
+#include "clock.h"
+
+#define PM_APBC     ( *( ( volatile unsigned int *)0x40000420 ) )
+
+/* 64 prescaler, 1MHz / 64 = 15625 */
+#define TCC0_SECOND_COUNT ( 0x3D09 )
+#define TCC0_DEFAULT_COUNT ( TCC0_SECOND_COUNT << 1U )
+
+static volatile timer_t * TCC0 = ( timer_t * ) TCC0_BASE;
+
+void Timer_Init( void )
+{
+    /* 1Mhz clock source, TCC0 */
+    Clock_ConfigureGCLK( 0x6, 0x5, 0x1A );
+
+    PM_APBC |= ( 0x1 << 8U );
+
+    TCC0->CTRLA |= ( 0x5 << 8 );
+    TCC0->CC0 = ( TCC0_DEFAULT_COUNT );
+
+    /* MFRQ Mode */
+    TCC0->WAVE |= ( 0x1 << 0U );
+
+    TCC0->CTRLA |= ( 0x1 << 12 ) | ( 0x1 << 11 );
+    TCC0->INTENSET |= ( 0x1 << 16 );
+}
+
+void Timer_Start( void )
+{
+    TCC0->CTRLA |= ( 0x1 << 1 );
+}
+
+void Timer_ClearInterrupt( void )
+{
+    TCC0->INTFLAG |= ( 0x1 << 16 );
+}
+
+void Timer_UpdatePeriod( uint8_t shift )
+{
+    TCC0->CC0 = ( TCC0_DEFAULT_COUNT >> shift );
+}
---
+++ common/timer.h
@@ -0,0 +1,64 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "util.h"
+
+typedef struct
+{
+    uint32_t CTRLA:32;
+    uint8_t CTRLBCLR:8;
+    uint8_t CTRLBSET:8;
+    uint8_t _RESERVED_0:8;
+    uint8_t _RESERVED_1:8;
+    uint32_t SYNCBUSY:32;
+    uint32_t FCTRLA:32;
+    uint32_t FCTRLB:32;
+    uint32_t WEXCTRL:32;
+    uint32_t DRVCTRL:32;
+    uint8_t _RESERVED_2:8;
+    uint8_t _RESERVED_3:8;
+    uint8_t DBGCTRL:8;
+    uint8_t _RESERVED_4:8;
+    uint32_t EVCTRL:32;
+    uint32_t INTENCLR:32;
+    uint32_t INTENSET:32;
+    uint32_t INTFLAG:32;
+    uint32_t STATUS:32;
+    uint32_t COUNT:32;
+    uint16_t PATT:16;
+    uint8_t _RESERVED_5:8;
+    uint8_t _RESERVED_6:8;
+    uint32_t WAVE:32;
+    uint32_t PER:32;
+    uint32_t CC0:32;
+    uint32_t CC1:32;
+    uint32_t CC2:32;
+    uint32_t CC3:32;
+    uint32_t _RESERVED_7:32;
+    uint32_t _RESERVED_8:32;
+    uint32_t _RESERVED_9:32;
+    uint8_t _RESERVED_10:8;
+    uint8_t _RESERVED_11:8;
+    uint8_t _RESERVED_12:8;
+    uint16_t PATTB:16;
+    uint8_t _RESERVED_13:8;
+    uint8_t _RESERVED_14:8;
+    uint32_t WAVEB:32;
+    uint32_t PERB:32;
+    uint32_t CCB0:32;
+    uint32_t CCB1:32;
+    uint32_t CCB2:32;
+    uint32_t CCB3:32;
+}
+timer_t;
+
+#define TCC0_BASE ( 0x42002000 )
+#define TCC1_BASE ( 0x42002400 )
+#define TCC2_BASE ( 0x42002800 )
+
+void Timer_Init( void );
+void Timer_Start( void );
+void Timer_ClearInterrupt( void );
+void Timer_UpdatePeriod( uint8_t shift );
+
+#endif /* TIMER_H */
